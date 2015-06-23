2b8c9e339a078f42d1552bd6a29f8fc95ed47c06
Paul Gomersbach <p.gomersbach@rely.nl>
2015-06-23 18:13:32 +0200

    update spec

--- files/spec/acceptance/class_spec.rb
+++ files/spec/acceptance/class_spec.rb
@@ -12,33 +12,33 @@ describe 'profile_jenkins class' do
   context 'default parameters' do
     if ENV['BEAKER'] == 'true'
       # Using puppet_apply as a helper
-      it 'should work idempotently with no errors' do
+      it 'should work with no errors' do
         pp = <<-EOS
         class { 'profile_jenkins': }
         EOS
 
-        # Run it twice and test for idempotency
         apply_manifest(pp, :catch_failures => true, :future_parser => true)
-        apply_manifest(pp, :catch_changes  => true, :future_parser => true)
       end
     end
 
 
   
 # a profile class should test if the included packages and services are installed, enabled and running. Please adept to your needs. See examples below:
-#   describe package('ntp') do
-#      it { is_expected.to be_installed }
-#    end
-#
-#    describe service('ntp') do
-#      it { is_expected.to be_enabled }
-#      it { is_expected.to be_running }
-#    end
-#
-#    describe port(5432) do
-#      it { should be_listening.with('tcp') }
-#    end
-  
+    describe package('jenkins') do
+      it { is_expected.to be_installed }
+    end
+
+    describe service('jenkins') do
+      it { is_expected.to be_enabled }
+      it { is_expected.to be_running }
+    end
+
+    describe port(8080) do
+      it {
+        sleep(10) # Jenkins takes a while to start up
+        should be_listening
+      }
+    end  
 
   end
 end
