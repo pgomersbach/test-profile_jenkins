49cf0aebd34414a806e292cac92e7c156bb8cd4b
Paul Gomersbach <p.gomersbach@rely.nl>
2015-06-24 16:31:30 +0200

    enable plugins

--- manifests/init.pp
+++ manifests/init.pp
@@ -9,54 +9,57 @@
 #
 class profile_jenkins
 {
-  class { 'docker': }
+
+  package { 'jq': }
+
   user { 'jenkins':
     groups  => 'docker',
-    require => Class['docker'],
-#    notify  => Service['jenkins'],
+    require => Class['docker', 'jenkins'],
   }
-  package { 'jq': }
+
+  class { 'docker': }
+
 #  jenkins::job { "${module_name}-start":
 #    config  => template("${module_name}/start.xml.erb"),
 #  }
 
-  class { '::jenkins':
+  class { 'jenkins':
     configure_firewall => false,
     cli                => true,
     plugin_hash        => {
-#      'jenkins-cloudformation-plugin' => {},
-#      'htmlpublisher'                 => {},
-#      'rundeck'                       => {},
-#      'parameterized-trigger'         => {},
-#      'multiple-scms'                 => {},
-      'git-client' => {},
-      'git'        => {},
-      'github'     => {},
-      'github-api' => {},
-#      'token-macro'                   => {},
-#      'scm-api'                       => {},
-#      'promoted-builds'               => {},
-#      'matrix-project'                => {},
-#      'run-condition'                 => {},
-#      'conditional-buildstep'         => {},
-#      'rebuild'                       => {},
-#      'maven-plugin'                  => {},
-#      'subversion'                    => {},
-#      'googlecode'                    => {},
-#      'ssh-credentials'               => {},
-#      'credentials'                   => {},
-#      'analysis-core'                 => {},
-#      'warnings'                      => {},
-#      'rvm'                           => {},
-#      'project-health-report'         => {},
-#      'greenballs'                    => {},
-#      'jquery'                        => {},
-#      'build-pipeline-plugin'         => {},
-#      'delivery-pipeline-plugin'      => {},
-#      'ruby'                          => {},
-#      'ruby-runtime'                  => {},
-#      'dynamic-axis'                  => {},
-#      'envinject'                     => {},
+      'jenkins-cloudformation-plugin' => {},
+      'htmlpublisher'                 => {},
+      'rundeck'                       => {},
+      'parameterized-trigger'         => {},
+      'multiple-scms'                 => {},
+      'git-client'                    => {},
+      'git'                           => {},
+      'github'                        => {},
+      'github-api'                    => {},
+      'token-macro'                   => {},
+      'scm-api'                       => {},
+      'promoted-builds'               => {},
+      'matrix-project'                => {},
+      'run-condition'                 => {},
+      'conditional-buildstep'         => {},
+      'rebuild'                       => {},
+      'maven-plugin'                  => {},
+      'subversion'                    => {},
+      'googlecode'                    => {},
+      'ssh-credentials'               => {},
+      'credentials'                   => {},
+      'analysis-core'                 => {},
+      'warnings'                      => {},
+      'rvm'                           => {},
+      'project-health-report'         => {},
+      'greenballs'                    => {},
+      'jquery'                        => {},
+      'build-pipeline-plugin'         => {},
+      'delivery-pipeline-plugin'      => {},
+      'ruby'                          => {},
+      'ruby-runtime'                  => {},
+      'dynamic-axis'                  => {},
+      'envinject'                     => {},
     },
   }
 }
