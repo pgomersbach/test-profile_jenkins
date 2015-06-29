853773383436907bf06d250d22dbc5641b01c1ce
Paul Gomersbach <p.gomersbach@rely.nl>
2015-06-29 17:43:11 +0200

    add plugin

--- manifests/init.pp
+++ manifests/init.pp
@@ -35,6 +35,7 @@ class profile_jenkins
       'git'                           => { version      => '1.4.0' },
       'github'                        => {},
       'github-api'                    => {},
+      'buildresult-trigger            => {},
 #      'token-macro'                   => {},
 #      'scm-api'                       => {},
 #      'promoted-builds'               => {},
