ad97e1c6f77a75a43be98f237b9b9d904fae7acb
Paul Gomersbach <p.gomersbach@rely.nl>
2015-07-15 19:59:27 +0200

    change package libxslt1-dev

--- manifests/init.pp
+++ manifests/init.pp
@@ -9,7 +9,7 @@
 #
 class profile_jenkins
 {
-  package { [ 'bundler' ,'libxslt-dev', 'libxml2-dev', 'zlib1g-dev']:
+  package { [ 'bundler' ,'libxslt1-dev', 'libxml2-dev', 'zlib1g-dev']:
     ensure => installed,
   }
 
