5bf5bd8d57d643491faaa5cca565c40a3cdecb27
Paul Gomersbach <p.gomersbach@rely.nl>
2015-07-17 15:14:26 +0200

    add rundeck job

--- manifests/jobs.pp
+++ manifests/jobs.pp
@@ -10,6 +10,7 @@ class profile_jenkins::jobs {
     'profile_puppetmaster' => { jobgiturl    => 'https://github.com/pgomersbach/test-profile_puppetmaster.git' },
     'profile_base' => { jobgiturl    => 'https://github.com/pgomersbach/test-profile_base.git' },
     'profile_mcollective' => { jobgiturl    => 'https://github.com/pgomersbach/test-profile_mcollective.git' },
+    'profile_rundeck' => { jobgiturl    => 'https://github.com/pgomersbach/test-profile_rundeck.git' },
     'role_deployserver' => { jobgiturl    => 'https://github.com/pgomersbach/test-role_deployserver.git' },
     'role_ciserver' => { jobgiturl    => 'https://github.com/pgomersbach/test-role_ciserver.git' },
     'rspec_monitor' => { jobgiturl    => 'https://github.com/pgomersbach/test-rspec_monitor.git' },
